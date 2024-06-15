e5e968e2519af260707a0caa99717859fc8e6bab
Lior Bas <look551@gmail.com>
2024-06-15 22:37:26 +0300

    update7

--- src/3-models/employees-model.ts
+++ src/3-models/employees-model.ts
@@ -1,4 +1,5 @@
 import { ValitationError } from "./client-error";
+import Joi from "joi";
 
 export class EmployeesModel {
   public id: number;
@@ -20,13 +21,37 @@ export class EmployeesModel {
     this.hireDate = employee.hireDate;
   }
 
-  public employeesValidate() {
-    if (!this.firstName) throw new ValitationError("Missing first name!");
-    if (!this.lastName) throw new ValitationError("Missing last name!");
-    if (!this.title) throw new ValitationError("Missing title!");
-    if (!this.titleOfCourtesy)
-      throw new ValitationError("Missing titleOfCourtesy");
-    if (!this.birthDate) throw new ValitationError("Missing birthDate!");
-    if (!this.hireDate) throw new ValitationError("Missing hireDate!");
+  private static insertValidationSchema = Joi.object({
+    id: Joi.number().forbidden(),
+    firstName: Joi.string().max(50).required(),
+    lastName: Joi.string().max(50).required(),
+    birthDate: Joi.string().isoDate().required(),
+  });
+  private static updateValidationSchema = Joi.object({
+    id: Joi.number().required().min(1).integer(),
+    firstName: Joi.string().max(50).required(),
+    lastName: Joi.string().max(50).required(),
+    birthDate: Joi.string().isoDate().required(),
+  });
+
+  public validateInsert(): void {
+    const result = EmployeesModel.insertValidationSchema.validate(this);
+    if (result.error) throw new ValitationError(result.error.message);
   }
+
+  // Validating current object against the update schema:
+  public validateUpdate(): void {
+    const result = EmployeesModel.updateValidationSchema.validate(this);
+    if (result.error) throw new ValitationError(result.error.message);
+  }
+
+  // public employeesValidate() {
+  // if (!this.firstName) throw new ValitationError("Missing first name!");
+  // if (!this.lastName) throw new ValitationError("Missing last name!");
+  // if (!this.title) throw new ValitationError("Missing title!");
+  // if (!this.titleOfCourtesy)
+  //   throw new ValitationError("Missing titleOfCourtesy");
+  // if (!this.birthDate) throw new ValitationError("Missing birthDate!");
+  // if (!this.hireDate) throw new ValitationError("Missing hireDate!");
+  // }
 }
